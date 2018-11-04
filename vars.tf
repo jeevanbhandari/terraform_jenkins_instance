f4629c52ecdbd39d29d4318b32749c98841f0a72
jeevanbhandari <bhandarijeevan89@gmail.com>
2018-11-04 19:03:34 +0000

    instance

---
+++ vars.tf
@@ -0,0 +1,16 @@
+variable "region"{}
+variable "instancetype"{}
+variable "count"{}
+
+variable "amis" {
+  type = "map"
+
+
+  default = {
+   eu-west-1 = "ami-6e28b517"
+   us-east-1 = "ami-4bf3d731"
+   us-west-2 = "ami-a042f4d8"
+
+  }
+
+}
